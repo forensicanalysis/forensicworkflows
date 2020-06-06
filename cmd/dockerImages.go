f589c7565276b675f91f1ecc7009cd87d604d631
Jonas Plum <cugu@users.noreply.github.com>
2020-06-07 01:40:03 +0200

    Add yara docker url (#37)

--- cmd/dockerImages.go
+++ cmd/dockerImages.go
@@ -25,4 +25,5 @@ var dockerImages = []string{ // nolint: gochecknoglobals
 	"forensicanalysis/elementary-shimcache:latest",
 	"forensicanalysis/elementary-plaso:latest",
 	"forensicanalysis/elementary-import-image:latest",
+	"forensicanalysis/elementary-yara:latest",
 }
